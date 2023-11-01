1aa5519bc74ea13bafc96de112b52d85a0e5f434
Abigail Alegre <alegreav@gmail.com>
2023-11-01 14:28:02 -0300

    Nav bar para facilitar navegacion

--- The-Path-of-Nuala/src/app/app-routing.module.ts
+++ The-Path-of-Nuala/src/app/app-routing.module.ts
@@ -3,11 +3,17 @@ import { RouterModule, Routes } from '@angular/router';
 import { RegisterComponent } from './pages/register/register.component';
 import { HomeComponent } from './pages/home/home.component';
 import { LoginComponent } from './pages/login/login.component';
+import { ContactUsComponent } from './pages/contact-us/contact-us.component';
+import { HowToPlayComponent } from './pages/how-to-play/how-to-play.component';
+import { PlayComponent } from './pages/play/play.component';
 
 const routes: Routes = [
   {path:'home',component:HomeComponent},
   {path:'login',component:LoginComponent},
   {path:'register',component:RegisterComponent},
+  {path:'contact-us', component:ContactUsComponent},
+  {path:'how-to-play', component:HowToPlayComponent},
+  {path:'play',component:PlayComponent},
   {path:'**',redirectTo:'home'}
 ];
 
